c726ae4bd65e5dadbb1537df921e10c5e7716486
Joseph Mazariegos <joseph.mazariegos7@gmail.com>
2026-04-26 22:26:51 -0700

    Added frontend task creation form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,103 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]); // state to store the list of tasks, initially empty array
+  const [title, setTitle] = useState(""); // state to store title of new task, initially empty string
+  const [description, setDescription] = useState(""); // state to store description of new task, initialy empty string
 
-  useEffect(() => {
-    fetch('http://127.0.01:8000/tasks')
+  function fetchTasks() {
+    fetch("http://localhost:8000/tasks")
       .then((response) => response.json())
       .then((data) => {
         setTasks(data);
       })
       .catch((error) => {
-        console.error('Error fetching tasks:', error);
+        console.error("Error fetching tasks:", error);
       });
+  }
+
+  useEffect(() => {
+    fetchTasks();
   }, []);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newTask = {
+      title: title,
+      description: description,
+    };
+
+    fetch("http://127.0.0.1:800/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setTitle("");
+        setDescription("");
+        fetchTasks();
+      })
+      .catch((error) => {
+        console.error("Error creating task:", error);
+      });
+  }
+
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial"}}>
+    <div style={{ padding: "2rem", frontFamily: "Arial" }}>
       <h1>AgentEval</h1>
+
+      <h2>Create Task</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title</label>
+          <br />
+          <input
+            tpye="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="e.g. Two Sum"
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Description</label>
+          <br />
+          <textarea
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            placeholder="e.g. Describe the task..."
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Create Task</button>
+      </form>
+
+      <hr />
+
       <h2>Tasks</h2>
 
-      {tasks.length === 0 ? (
+      {tasks.length == 0 ? (
         <p>No tasks found.</p>
       ) : (
         <ul>
           {tasks.map((task) => (
             <li key={task.id}>
               <strong>{task.title}</strong> - {task.description}
-              </li>
+            </li>
           ))}
         </ul>
       )}
     </div>
-  )
+  );
 }
 
 export default App;
